510011122717e75d7984325f7c805d558d2d530e
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-25 16:55:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,11 +15,11 @@ monopoly
 end
 
 def monopoly_with_second_tier
-			monopoly = {
+			#monopoly = {
 			  
 			}
-	  monopoly[:railroads] = {}
-monopoly
+	  #monopoly[:railroads] = {}
+#monopoly
 	  
  #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
